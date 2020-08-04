d7204a6cff7d6066e413072d82d7377493638d19
ingindIsrael <ing.visrael@gmail.com>
2020-08-04 12:52:33 -0400

    error user unkown
    en forms.py me da un error django.core.exceptions.FieldError: Unknown field(s) (user) specified for Post

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -14,9 +14,13 @@ urlpatterns = [
     #path('hiyou/<str:name>/<int:age>/',views.say_hi),
 
     path('admin/', admin.site.urls),
+
     path('users/login/', users_views.login_view, name='login'),
     path('users/logout/', users_views.logout_view, name='logout'),
-    path('posts/', posts_views.list_posts, name='feed'),
+
+    path('', posts_views.list_posts, name='feed'),
+    path('posts/new/', posts_views.create_post, name='create_post'),
+    
     path('users/signup/', users_views.signup, name='signup'),
     path('users/me/profile/', users_views.update_profile, name='update_profile')
 
